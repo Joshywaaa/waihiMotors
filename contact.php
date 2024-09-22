f849a9cd8dc362bfbf6902f523f1204524abcb90
Joshywaaa <crappycam3464@gmail.com>
2024-09-22 14:48:09 +1200

    nav improvement

--- contact.php
+++ contact.php
@@ -12,32 +12,7 @@
 <body>
 
   <!-- Navbar -->
-  <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <a class="navbar-brand" href="#">Waihi Motors</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNav">
-      <ul class="navbar-nav ml-auto">
-        <li class="nav-item active">
-          <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="about.php">About</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="services.php">Services</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="contact.php">Contact</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="reviews.php">Reviews</a>
-        </li>
-      </ul>
-    </div>
-  </nav>
-
+  <?php include includes/nav.php ?>
 
   <!-- Footer -->
   <footer class="text-center mt-4">
